f1ffa8515d9c1c8b1fe53b13979647a2100ead36
js0601 <73793326+js0601@users.noreply.github.com>
2025-03-20 13:58:26 +0100

    make methods public

--- src/chunk.rs
+++ src/chunk.rs
@@ -21,7 +21,7 @@ pub struct Chunk {
 
 #[allow(dead_code)] // NOTE: needed?
 impl Chunk {
-    fn new(chunk_type: ChunkType, data: Vec<u8>) -> Chunk {
+    pub fn new(chunk_type: ChunkType, data: Vec<u8>) -> Chunk {
         // NOTE: if the message is longer than 4.2 million characters this will panic
         // but unless you are going to encode War and Peace ~1.4 times this shouldn't happen
         let length = data
@@ -40,27 +40,27 @@ impl Chunk {
         }
     }
 
-    fn length(&self) -> u32 {
+    pub fn length(&self) -> u32 {
         self.length
     }
 
-    fn chunk_type(&self) -> &ChunkType {
+    pub fn chunk_type(&self) -> &ChunkType {
         &self.chunk_type
     }
 
-    fn data(&self) -> &[u8] {
+    pub fn data(&self) -> &[u8] {
         &self.data
     }
 
-    fn crc(&self) -> u32 {
+    pub fn crc(&self) -> u32 {
         self.crc
     }
 
-    fn data_as_string(&self) -> crate::Result<String> {
+    pub fn data_as_string(&self) -> crate::Result<String> {
         Ok(<String>::from_utf8(self.data().to_vec())?)
     }
 
-    fn as_bytes(&self) -> Vec<u8> {
+    pub fn as_bytes(&self) -> Vec<u8> {
         self.length
             .to_be_bytes()
             .iter()
