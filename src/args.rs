c610635a9248767757437cab810a98f516a68d6d
js0601 <73793326+js0601@users.noreply.github.com>
2025-03-21 14:45:43 +0100

    add command line functionality

---
+++ src/args.rs
@@ -0,0 +1,65 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "PNGecret", version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Option<PngecretArgs>,
+}
+
+#[derive(Subcommand)]
+pub enum PngecretArgs {
+    /// Encode a secret message in a PNG file
+    Encode(EncodeArgs),
+
+    /// Decode a secret message from a PNG file
+    Decode(DecodeArgs),
+
+    /// Remove a secret message from a PNG file
+    Remove(RemoveArgs),
+
+    /// Print all chunks in a PNG file
+    Print(PrintArgs),
+}
+
+#[derive(Parser, Debug)]
+pub struct EncodeArgs {
+    /// PNG file to hide message in
+    file: PathBuf,
+
+    /// Chunk Type where message is hidden  
+    /// (for more info, look at the PNG structure doc)
+    chunk_type: String,
+
+    /// Message to hide
+    msg: String,
+
+    /// Optional output file
+    output: Option<PathBuf>,
+}
+
+#[derive(Parser, Debug)]
+pub struct DecodeArgs {
+    /// PNG file to decode hidden message from
+    file: PathBuf,
+
+    /// Type of chunk the message is hidden in
+    chunk_type: String,
+}
+
+#[derive(Parser, Debug)]
+pub struct RemoveArgs {
+    /// PNG file to remove message from
+    file: PathBuf,
+
+    /// Type of chunk the message is hidden in
+    chunk_type: String,
+}
+
+#[derive(Parser, Debug)]
+pub struct PrintArgs {
+    /// PNG file to print
+    file: PathBuf,
+}
