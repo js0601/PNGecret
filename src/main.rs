90647201fa66627b2524800259ff815eebd3728b
js0601 <73793326+js0601@users.noreply.github.com>
2025-03-22 15:29:36 +0100

    have main call commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use args::{Cli, RemoveArgs};
+use clap::Parser;
+use command::*;
+
 mod args;
 mod chunk;
 mod chunk_type;
@@ -8,5 +12,16 @@ pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    todo!()
+    let cli = Cli::parse();
+
+    if let Some(c) = cli.command {
+        match c {
+            args::PngecretArgs::Encode(encode_args) => encode(encode_args)?,
+            args::PngecretArgs::Decode(decode_args) => decode(decode_args)?,
+            args::PngecretArgs::Remove(remove_args) => remove(remove_args)?,
+            args::PngecretArgs::Print(print_args) => print(print_args)?,
+        }
+    }
+
+    Ok(())
 }
